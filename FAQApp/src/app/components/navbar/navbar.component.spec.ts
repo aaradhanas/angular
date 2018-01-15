e3b4f2b8618d2dbee7bf8befa41201faa7805118
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-01-15 22:35:00 +0530

    Tests for navbar component

--- FAQApp/src/app/components/navbar/navbar.component.spec.ts
+++ FAQApp/src/app/components/navbar/navbar.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core'
+import { By } from '@angular/platform-browser'
 
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +20,19 @@ describe('NavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
+
+    de = fixture.debugElement;
+    de = de.query(By.css('a'));
+    el = de.nativeElement;
+
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Check title', () => {
+    expect(el.textContent).toEqual('FAQ App');
+});
 });
