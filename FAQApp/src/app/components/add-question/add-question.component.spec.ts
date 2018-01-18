b9cc9dfef2b640e5744fa610ac5a1e0f100c4b2f
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-01-18 22:23:42 +0530

    Added unit testd for add question

--- FAQApp/src/app/components/add-question/add-question.component.spec.ts
+++ FAQApp/src/app/components/add-question/add-question.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AddQuestionComponent } from './add-question.component';
+import { Question } from '../../models/Question';
 
 describe('AddQuestionComponent', () => {
   let component: AddQuestionComponent;
   let fixture: ComponentFixture<AddQuestionComponent>;
-
+  let de: DebugElement;
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddQuestionComponent ]
+      declarations: [ AddQuestionComponent ],
+      imports : [ FormsModule ]
     })
     .compileComponents();
   }));
@@ -21,5 +27,27 @@ describe('AddQuestionComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('raise question added event', () => {
+
+    de = fixture.debugElement.query(By.css('.question'));
+    const questionEl = de.nativeElement;
+    questionEl.value = 'What is your name?';
+
+    de = fixture.debugElement.query(By.css('.answer'));
+    const answerEl = de.nativeElement;
+    answerEl.value = 'Aara';
+
+    let addedQuestion: Question;
+    component.questionAdded.subscribe((question: Question) =>  {
+      addedQuestion = question;
+      expect(addedQuestion.text).toEqual(questionEl.value);
+      expect(addedQuestion.answer).toEqual(answerEl.value);
+    });
+
+    de = fixture.debugElement.query(By.css('.btn'));
+
+    de.triggerEventHandler('click', null);
   });
 });
