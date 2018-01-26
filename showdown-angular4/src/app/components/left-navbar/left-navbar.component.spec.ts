d5a1e6001b2821a1da267709e901a0d9bc8774de
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-01-26 23:48:31 +0530

    Unit tests for left nav bar

--- showdown-angular4/src/app/components/left-navbar/left-navbar.component.spec.ts
+++ showdown-angular4/src/app/components/left-navbar/left-navbar.component.spec.ts
@@ -1,14 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LeftNavbarComponent } from './left-navbar.component';
+import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs/Rx';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('LeftNavbarComponent', () => {
   let component: LeftNavbarComponent;
   let fixture: ComponentFixture<LeftNavbarComponent>;
+  let dataServiceStub;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
+
+     dataServiceStub = {
+      leftVisibleChange: new EventEmitter(),
+      isLeftVisible() {
+        return false;
+      },
+      getVersions() {
+        // not necessary to populate data
+         return Observable.create( observer => {
+           observer.next(['1.0.0', '2.0.0']);
+           observer.complete();
+         });
+      },
+      getOptions() {
+        return {
+          'checkOpts': { 'omitExtraWLInCodeBlocks': true },
+          'numOpts': { 'headerLevelStart': 3 },
+          'textOpts': { 'prefixHeaderId' : 'p1' }
+        };
+      }
+    };
     TestBed.configureTestingModule({
-      declarations: [ LeftNavbarComponent ]
+      declarations: [ LeftNavbarComponent ],
+      providers : [{ provide: DataService, useValue: dataServiceStub}],
+      imports : [ FormsModule, BrowserAnimationsModule ]
     })
     .compileComponents();
   }));
@@ -21,5 +54,67 @@ describe('LeftNavbarComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('check invisible', () => {
+    component.state  = 'invisible';
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('.lateral-menu'));
+    el = de.nativeElement;
+    expect(el.style.display).toBe('none');
+  });
+
+  it('check visible', () => {
+    component.state = 'visible';
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('.lateral-menu'));
+    el = de.nativeElement;
+    expect(el.style.display).toBe('');
+  });
+
+  it('verify active version', () => {
+    spyOn(component, 'onVersionChange');
+
+    de = fixture.debugElement.query(By.css('select'));
+    el = de.nativeElement;
+    // de.triggerEventHandler('change', {}); => Does not work !!
+    el.dispatchEvent(new Event('change'));
+
+    expect(component.onVersionChange).toHaveBeenCalled();
+
+  });
+
+  it('verify check opts change', () => {
+    spyOn(component, 'checkValueChanged');
+
+    const key = 'omitExtraWLInCodeBlocks';
+    de = fixture.debugElement.query(By.css('#checkOpts-' + key));
+    el = de.nativeElement;
+
+    // expect(element.value).toEqual('on');
+    el.dispatchEvent(new Event('change'));
+    expect(component.checkValueChanged).toHaveBeenCalled();
+  });
+
+  it('verify num opts change', () => {
+    spyOn(component, 'numValueChanged');
+
+    de = fixture.debugElement.query(By.css('#numOpts-headerLevelStart'));
+    el = de.nativeElement;
+    // expect(element.value).toBe('3');
+
+    el.dispatchEvent(new Event('input'));
+    expect(component.numValueChanged).toHaveBeenCalled();
+  });
+
+  it('verify text opts change', () => {
+    spyOn(component, 'textValueChanged');
+
+    de = fixture.debugElement.query(By.css('#textOpts-prefixHeaderId'));
+    el = de.nativeElement;
+
+    // TODO expect(el.textContent).toEqual('p1');
+    el.dispatchEvent(new Event('input'));
+    expect(component.textValueChanged).toHaveBeenCalled();
   });
 });
